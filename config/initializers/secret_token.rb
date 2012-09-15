6c93c7736a868bdf9d25acbeefeba7aae9b3bea1
Priyanka Jalisatgi <priyankagj@gmail.com>
2012-09-15 18:31:11 -0400

    First commit-login

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackChannelApplication::Application.config.secret_token = '24f3ef7983d3d0af80334c416a65110deb4393bbb2872df82e02ebfb7f17190d4b755ef1ac05f1d94c3c428481cc5c00ce24ea9ae85b751247b8d38fde89bece'
